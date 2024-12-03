bf4d305d4f3aa0bc9e2445a26a22863e338bb639
KoKyungBeom <kyungbum6805@gmail.com>
2024-12-03 18:53:16 +0900

    update SecurityConfiguration.yml

--- server/src/main/java/com/springboot/config/SecurityConfiguration.java
+++ server/src/main/java/com/springboot/config/SecurityConfiguration.java
@@ -64,6 +64,7 @@ public class SecurityConfiguration {
                 .accessDeniedHandler(new MemberAccessDeniedHandler())
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(HttpMethod.GET,"/health").permitAll()
                         .antMatchers(HttpMethod.GET,"/h2").permitAll()
                         .antMatchers(HttpMethod.POST,"/h2/**").permitAll()
                         .antMatchers(HttpMethod.GET,"/h2/**").permitAll()
@@ -116,7 +117,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://dg3og7cz1f8xb.cloudfront.net","http://meetbti-client.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://d566icsg98vq7.cloudfront.net","http://meetbti-client.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization","MemberId"));
